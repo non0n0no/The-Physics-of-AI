252e82c7ef5a5006b408d6e2e9d2f9bf4285d809
non0n0no <a55120@alunos.ipb.pt>
2024-11-18 04:31:14 +0000

    Velocity

---
+++ Assets/Scripts/SecondsUpdate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondsUpdate : MonoBehaviour
+{
+
+    float timeStartOffSet = 0;
+    bool gotStartTime = false;
+
+    [SerializeField] private float speed = 0.5f;
+
+    void Update()
+    {
+        if (!gotStartTime)
+        {
+            timeStartOffSet = Time.realtimeSinceStartup;
+            gotStartTime = true;
+        }
+        this.transform.position = new Vector3(this.transform.position.x,
+                                              this.transform.position.y,
+                                              (Time.realtimeSinceStartup - timeStartOffSet) * speed);
+
+
+    }
+}
---
+++ Assets/Scripts/UpdateMove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class UpdateMove : MonoBehaviour
+{
+
+    [SerializeField] private float speed = 0.5f;
+
+    void Update()
+    {
+        this.transform.Translate(0,0,Time.deltaTime * speed);
+    }
+}
