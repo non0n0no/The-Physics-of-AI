6b9559e9a35d380d9e98364001848b977334126d
non0n0no <a55120@alunos.ipb.pt>
2024-11-18 04:36:05 +0000

    Calculating Trajectories

--- Assets/Scripts/MoveShell.cs
+++ Assets/Scripts/MoveShell.cs
@@ -1,22 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Timeline;
-using UnityEditor.UIElements;
 using UnityEngine;
 
-public class MoveShell : MonoBehaviour
-{
-    [SerializeField] private float speed = 1f;
+public class MoveShell : MonoBehaviour {
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 1.0f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        this.transform.Translate(0, Time.deltaTime * (speed / 2), Time.deltaTime * speed);
+
+    void Update() {
+
+        transform.Translate(0.0f, 0.0f, Time.deltaTime * speed);
     }
 }
